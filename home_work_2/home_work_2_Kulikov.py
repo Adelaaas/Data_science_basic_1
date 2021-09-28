61fb0d6d2c1e4a1ef00ef7ade67525e9bff7df3a
MoxDevl <90704520+MoxDevl@users.noreply.github.com>
2021-09-28 16:35:34 +0300

    Add files via upload

--- home_work_2/home_work_2_Kulikov.py
+++ home_work_2/home_work_2_Kulikov.py
@@ -8,8 +8,8 @@ def distance(x1, y1, x2, y2):
     si = (((x1 - x2)**2)+((y1 - y2)**2))**0,5
   return si
 # Задача 2
-def season(month)
-  s global
+def season(month):
+  global s
   if month == 12 or month == 1 or month == 2:
     s = 'Зима'
   elif month == 3 or month == 4 or month == 5:
@@ -18,13 +18,18 @@ def season(month)
     s = 'Лето'
   elif month == 9 or month == 10 or month == 11:
     s = 'Осень'
+  print(s)
 # Задача 4
-a = list(input().split(', '))
-b = []
-l = int(len(a))
-for i in range(l-1):
-  print(a[l-i-1], end=', ')
-print(a[0])
+def invert(a):
+  a = list(a)
+  b = []
+  l = int(len(a))
+  for i in range(l-1):
+    print(a[l-i-1], end=', ')
+    b.append(a[l-i-1])
+  print(a[0])
+  b.append(a[0])
+  return b
 # Задача 5
 hello = list('Привет мир!')
 bye = []
